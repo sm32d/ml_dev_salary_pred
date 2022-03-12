663689dbedf6c1f315eb40a82db9d6aba69cbc7c
Dominik Mehta <dominik@Dominiks-MacBook-Air.local>
2022-03-12 18:25:34 +0530

    Added requirements

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
-mkdir -p ~/.streamlit/echo "\
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"streamlit@sulabh.ru\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
 " > ~/.streamlit/config.toml
